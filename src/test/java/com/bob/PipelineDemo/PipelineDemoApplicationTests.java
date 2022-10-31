4af61a588d36424252d501b3538148bd0430ebba
Ryan Tang <ryan.tang@softmobile.com.tw>
2022-10-31 17:07:39 +0800

    add spring-boot

---
+++ src/test/java/com/bob/PipelineDemo/PipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.PipelineDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
